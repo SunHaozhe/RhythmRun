fd99da871dd060ae144f767a1d3758586226ef42
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-11-23 09:30:11 +0100

    Exemple de commentaires /!\ non définitif

--- app/src/main/java/com/example/raphaelattali/rythmrun/MainActivity.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/MainActivity.java
@@ -6,17 +6,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//EXEMPLE ANDROID
+
 public class MainActivity extends AppCompatActivity {
 
+    // Initialisation d'un bouton
     Button button = null;
 
+    //Methode appelée au lancement de l'activité
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        // le bouton est associé à celui sur l'interface graphique
         button = (Button)findViewById(R.id.button);
+        // le bouton réagit au clic et envoie sur l'activité Main2Activity
+        // Main2Activity n'est pas définitive
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
