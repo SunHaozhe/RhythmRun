0201e3e3db25cb40730a61ac8190742eb31c2d51
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-12-01 17:13:30 +0100

    DataSaveInterface déclarée, elle est vide pour le moment

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/data/DataSaveInterface.java
@@ -0,0 +1,14 @@
+package com.example.raphaelattali.rythmrun.interfaces.data;
+
+/**
+ * Created by Raphael Attali on 01/12/2016.
+ */
+
+/**
+ * Interface liée aux sauvegardes dans la mémoire du téléphone
+ */
+
+
+public interface DataSaveInterface {
+
+}
