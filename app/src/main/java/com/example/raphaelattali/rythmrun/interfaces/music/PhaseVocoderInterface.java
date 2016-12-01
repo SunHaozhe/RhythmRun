07730112fa22671ec467758247a2811941a23f4a
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-12-01 16:41:09 +0100

    PhaseVocoderInterface et TempoInterface implémentées correctement Des commentaires expliquent les différentes fonctions

--- app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/music/PhaseVocoderInterface.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/music/PhaseVocoderInterface.java
@@ -4,6 +4,8 @@ package com.example.raphaelattali.rythmrun.interfaces.music;
  * Created by Raphael Attali on 01/12/2016.
  */
 
+import com.example.raphaelattali.rythmrun.music.Music;
+
 /**
  * Interface liée au vocodeur de phase
  * Il faut qu'il puisse modifier la musique et la renvoyer
@@ -11,5 +13,10 @@ package com.example.raphaelattali.rythmrun.interfaces.music;
  */
 
 public interface PhaseVocoderInterface {
-    //
+    /**
+     * Doit renvoyer une musique modifiée, telle que le tempo soit désormais celui demandé
+     * On donne en argument le rythme (en bmp) et la musique d'origine, et la fonction la transforme et la renvoie
+     */
+    public Music modifyMusicToFitTempo(Music music, float tempo);
+
 }
--- app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/music/TempoInterface.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/music/TempoInterface.java
@@ -4,5 +4,21 @@ package com.example.raphaelattali.rythmrun.interfaces.music;
  * Created by Raphael Attali on 01/12/2016.
  */
 
+import com.example.raphaelattali.rythmrun.music.Music;
+
+/**
+ * Interface liée à la gestion du tempo dans une musique
+ */
+
+
 public interface TempoInterface {
+    /**
+     * On peut récupérer la musique dans un fichier fileName et renvoyer son tempo en bmp
+     */
+    public float determineTempoOf(String fileName);
+
+    /**
+     * On peut récupérer une musique de classe Music et renvoyer son tempo en bmp
+     */
+    public float determineTempoOf(Music music);
 }
