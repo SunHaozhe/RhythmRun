9d8c60bd3e6906144abf90ef378faf97f1e91b17
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-12-01 15:58:35 +0100

    MainActivity and Main2Activity are now in package activities

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/activities/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.example.raphaelattali.rythmrun.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.raphaelattali.rythmrun.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
