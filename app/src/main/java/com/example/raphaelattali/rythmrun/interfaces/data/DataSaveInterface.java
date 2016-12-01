863f5732242c194e9be6dc8ed8c11328fe314c15
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-12-01 17:28:26 +0100

    Fonctions implémentées dans DataSaveInterface

--- app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/data/DataSaveInterface.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/interfaces/data/DataSaveInterface.java
@@ -8,7 +8,27 @@ package com.example.raphaelattali.rythmrun.interfaces.data;
  * Interface liée aux sauvegardes dans la mémoire du téléphone
  */
 
+import android.media.Image;
+
+/**
+ * Les fonctions de cette interface ont été trouvées avec l'aide de Haozhe Sun
+ */
 
 public interface DataSaveInterface {
+    // encoder et sauvegarder dans la mémoire une image
+    public void imageDataEncoder(String pictureName);
+
+    //encoder et sauvegarder dans la mémoire des informations chiffrées (nombre entier)
+    public void intDataEncoder(int dataNumber);
+
+    // renvoie l'image appelée fileName située dans la mémoire
+    public Image imageDataDecoder(String fileName);
+
+    // renvoie le nombre entier avec l'identifiant numberName
+    public int intDataDecoder(String numberName);
+
+    // Déterminer le nombre de données maximales enregistrées
+    public void setDataMax(int dataMax);
+
 
 }
