4912d97613af339d71f9798db71d2fa232b41da3
Raphael Attali <raphael.attali@telecom-paristech.fr>
2016-12-07 10:51:07 +0100

    Nouvelle méthode decompose(PeriodicWave rawWave) Elle ne fonctionne pas encore
    La Javadoc est écrite

--- app/src/main/java/com/example/raphaelattali/rythmrun/music/phase_vocoder/FourierTransform.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/music/phase_vocoder/FourierTransform.java
@@ -4,11 +4,13 @@ package com.example.raphaelattali.rythmrun.music.phase_vocoder;
  * Created by Raphael Attali on 07/12/2016.
  */
 
+import java.util.List;
+
 /**
  * La classe possède tous les outils pour faire une transformée de Fourier
  * Elle passe d'un signal périodique à une liste de sinusoïdes, et vice versa
  *
- * @see Sinus
+ * @see SinusWave
  *
  * @author Raphael Attali
  * @version 1.0
@@ -17,6 +19,16 @@ package com.example.raphaelattali.rythmrun.music.phase_vocoder;
 public class FourierTransform {
 
     /**
+     * <b>Permet de décomposer une onde périodique en une somme d'ondes sinusoidales</b>
      *
+     * @param rawWave
+     *      L'onde périodique brute, à décomposer
+     * @return La liste des ondes sinuoidales qui composent l'onde initiale
      */
+    public List<SinusWave> decompose(PeriodicWave rawWave){
+
+        // Ne fonctionne pas encore
+        return null;
+    }
+
 }
