b0a9f042f0de72e0cd2d8650a78c1caeecbcebfa
Lucas <legolucas@hotmail.fr>
2016-12-12 16:45:03 +0100

    Test de l'accelerometre : le boutton de main2activity affiche l'acceleration delon x en temps reel. On l'enregistre dans un fichier pour faire des tests avec.

--- app/src/main/java/com/example/raphaelattali/rythmrun/activities/Main2Activity.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/activities/Main2Activity.java
@@ -1,15 +1,24 @@
 package com.example.raphaelattali.rythmrun.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.raphaelattali.rythmrun.R;
+import com.example.raphaelattali.rythmrun.sensors.Accelerometer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
 
 public class Main2Activity extends AppCompatActivity {
 
     Button test_button = null;
+    int k = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +30,49 @@ public class Main2Activity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 test_button.setText("hop");
+                k = 1;
             }
         });
+
+        final Context mContext = this;
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Accelerometer acc = new Accelerometer(0.1f, 10, mContext);
+                OutputStreamWriter o;
+                try {
+                    o = new OutputStreamWriter(mContext.openFileOutput("donnees.txt", Context.MODE_WORLD_READABLE));
+
+
+                    while (k == 0) {
+                        final String x = String.valueOf(acc.getAx());
+                        o.write(x);
+                        o.write(" ");
+                        test_button.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                test_button.setText(x);
+                            }
+
+
+                        });
+                        try {
+                            Thread.sleep(30);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                    o.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+        }
+
+
     }
-}
--- app/src/main/java/com/example/raphaelattali/rythmrun/sensors/Accelerometer.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/sensors/Accelerometer.java
@@ -1,8 +1,10 @@
 package com.example.raphaelattali.rythmrun.sensors;
 
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 
 import java.util.Date;
 
@@ -20,20 +22,33 @@ public class Accelerometer implements SensorEventListener {
     private float periode;
     private float[][] valeurs;
     float ax, ay, az;
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+    public float getAx() {
+        return ax;
+    }
 
     /**
      *
      * @param periode
      * @param nombre_echantillons
      */
-    public Accelerometer(float periode, int nombre_echantillons) {
+    public Accelerometer(float periode, int nombre_echantillons, Context context) {
         this.periode = periode;
         frequence_echantillonage = 1/periode;
         this.nombre_echantillons = nombre_echantillons;
         this.plage_echantillonage = nombre_echantillons/frequence_echantillonage;
         valeurs = new float[3][nombre_echantillons];
-        ax = 0; ay = 0; az = 0;
-        completeTab(periode);
+        ax = 21f; ay = 0; az = 0;
+        mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        if (mSensor == null) {
+            ax = 42.0f;
+        }
+
+        //completeTab(periode);
     }
 
     /**
@@ -48,7 +63,7 @@ public class Accelerometer implements SensorEventListener {
         this.nombre_echantillons = (int) (plage_echantillonage*frequence_echantillonage);
         valeurs = new float[3][nombre_echantillons];
         ax = 0; ay = 0; az = 0;
-        completeTab(periode);
+        //completeTab(periode);
     }
 
     /**
@@ -86,8 +101,8 @@ public class Accelerometer implements SensorEventListener {
             long temps_debut;
             @Override
             public void run() {
-                temps_debut = Date.getTime();
-                
+                temps_debut = date.getTime();
+                valeurs[0][0] = (float)0.0;
             }
         });
         thread.start();
