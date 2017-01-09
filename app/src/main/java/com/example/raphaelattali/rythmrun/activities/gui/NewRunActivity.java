22b89232e1a4aff00aaa64c3b1a6877de841725e
ychalier <yohan.chalier@telecom-paristech.fr>
2017-01-09 10:40:41 +0100

    Ajout de l'activité NewRun. Ajout des ongles (fragments) Itinerary, Pace et Music.

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/activities/gui/NewRunActivity.java
@@ -0,0 +1,102 @@
+package com.example.raphaelattali.rythmrun.activities.gui;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.raphaelattali.rythmrun.R;
+
+import layout.ItineraryFragment;
+import layout.PaceFragment;
+import values.MusicFragment;
+
+public class NewRunActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_run);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.vpNewRun);
+        tabLayout = (TabLayout) findViewById(R.id.tlNewRun);
+
+        // Fragment manager to add fragment in viewpager we will pass object of Fragment manager to adpater class.
+        FragmentManager manager=getSupportFragmentManager();
+
+        //object of PagerAdapter passing fragment manager object as a parameter of constructor of PagerAdapter class.
+        PagerAdapter adapter=new PagerAdapter(manager);
+
+        //set Adapter to view pager
+        viewPager.setAdapter(adapter);
+
+        //set tablayout with viewpager
+        tabLayout.setupWithViewPager(viewPager);
+
+        // adding functionality to tab and viewpager to manage each other when a page is changed or when a tab is selected
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        //Setting tabs from adpater
+        tabLayout.setTabsFromPagerAdapter(adapter);
+
+    }
+
+    public class PagerAdapter extends FragmentStatePagerAdapter{
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Fragment frag=null;
+            switch (position){
+                case 0:
+                    frag=new ItineraryFragment();
+                    break;
+                case 1:
+                    frag=new PaceFragment();
+                    break;
+                case 2:
+                    frag=new MusicFragment();
+                    break;
+            }
+            return frag;
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title=" ";
+            switch (position){
+                case 0:
+                    title="Itinerary";
+                    break;
+                case 1:
+                    title="Pace";
+                    break;
+                case 2:
+                    title="Music";
+                    break;
+            }
+            return title;
+        }
+    }
+
+}
