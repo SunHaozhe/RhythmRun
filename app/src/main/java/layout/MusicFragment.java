1e067132aadde071a1af593b71d281c4104f550c
ychalier <yohan.chalier@telecom-paristech.fr>
2017-01-09 13:44:52 +0100

    Dans Music, cocher la case Random disable la liste déroulante.

--- app/src/main/java/layout/MusicFragment.java
+++ app/src/main/java/layout/MusicFragment.java
@@ -14,20 +14,35 @@ import com.example.raphaelattali.rythmrun.R;
 
 public class MusicFragment extends Fragment {
 
+    private CheckBox checkBox;
+    private Spinner spinner;
 
     public MusicFragment() {
         // Required empty public constructor
     }
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_music, container, false);
-        Spinner spinner = (Spinner) rootView.findViewById(R.id.spMusic);
+
+        spinner = (Spinner) rootView.findViewById(R.id.spMusic);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(), R.array.music_styles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        checkBox = (CheckBox) rootView.findViewById(R.id.cbMusic);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox.isChecked()){
+                    spinner.setEnabled(false);
+                }
+                else{
+                    spinner.setEnabled(true);
+                }
+            }
+        });
+
         return rootView;
     }
 
