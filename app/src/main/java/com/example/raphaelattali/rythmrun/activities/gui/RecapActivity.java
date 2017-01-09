ceffa01c590b0a25653c859ee2deadc32cf9ae03
ychalier <yohan.chalier@telecom-paristech.fr>
2017-01-09 15:14:22 +0100

    Ajout du message d'avertissement pour le recap avec une légère animation.

--- app/src/main/java/com/example/raphaelattali/rythmrun/activities/gui/RecapActivity.java
+++ app/src/main/java/com/example/raphaelattali/rythmrun/activities/gui/RecapActivity.java
@@ -3,6 +3,13 @@ package com.example.raphaelattali.rythmrun.activities.gui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.raphaelattali.rythmrun.R;
@@ -11,10 +18,15 @@ import layout.PaceFragment;
 
 public class RecapActivity extends AppCompatActivity {
 
+    private boolean shownWarning = false;
+    LinearLayout linearLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recap);
+
+        linearLayout = (LinearLayout) findViewById(R.id.RecapToHide);
 
         TextView tvDistance = (TextView) findViewById(R.id.tvRecapDistance);
         TextView tvPace = (TextView) findViewById(R.id.tvRecapPace);
@@ -34,5 +46,69 @@ public class RecapActivity extends AppCompatActivity {
         }
         tvMusic.setText(music);
 
+        Button button = (Button) findViewById(R.id.buttonRecapGO);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleWarning();
+            }
+        });
+
     }
+
+    public boolean toggleWarning(){
+        shownWarning = !shownWarning;
+        Log.i("toggle","Entering the toggle method");
+        TranslateAnimation a;
+
+        if(shownWarning){
+            Log.i("toggle","Opening the menu");
+            a = new TranslateAnimation(-linearLayout.getWidth(),0,0,0);
+            a.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    linearLayout.setVisibility(View.VISIBLE);
+                    Log.i("animation","Start of opening animation.");
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    Log.i("animation","End of opening animation");
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }
+        else{
+            Log.i("toggle","Closing the menu");
+            a = new TranslateAnimation(0,-linearLayout.getWidth(),0,0);
+            a.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                    Log.i("animation","Start of closing animation");
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    linearLayout.setVisibility(View.GONE);
+                    Log.i("animation","End of closing animation");
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+        }
+
+        a.setDuration(300);
+        a.setInterpolator(new AccelerateInterpolator());
+        linearLayout.startAnimation(a);
+
+        return shownWarning;
+    }
+
 }
