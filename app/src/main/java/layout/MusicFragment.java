f2ef9a58bca03e0b2de6674b5da40633128bda09
ychalier <yohan.chalier@telecom-paristech.fr>
2017-01-09 12:57:07 +0100

    Implémentation du layout pour le fragment Music de New Run.

--- app/src/main/java/layout/MusicFragment.java
+++ app/src/main/java/layout/MusicFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
 
 import com.example.raphaelattali.rythmrun.R;
 
@@ -20,8 +23,23 @@ public class MusicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_music, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_music, container, false);
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.spMusic);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(rootView.getContext(), R.array.music_styles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        return rootView;
+    }
+
+    public String getMusicStyle(){
+        Spinner spinner = (Spinner) this.getView().findViewById(R.id.spMusic);
+        CheckBox cb = (CheckBox) this.getView().findViewById(R.id.cbMusic);
+        if(cb.isChecked()){
+            return "Random";
+        }
+        else{
+            return (String) spinner.getSelectedItem();
+        }
     }
 
 }
