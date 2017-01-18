8eeaa742fdd6437e93b872ec1b02648a762b7d12
haozhe-sun <haozhe.sun@telecom-paristech.fr>
2017-01-18 23:23:18 +0100

    Creation of bluetoothActivity and its layout

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/activities/gui/BluetoothActivity.java
@@ -0,0 +1,19 @@
+package com.example.raphaelattali.rythmrun.activities.gui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.raphaelattali.rythmrun.R;
+
+/**
+ * Created by sun-haozhe on 18/01/2017.
+ */
+
+public class BluetoothActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+
+}
