624b21f18fac69fa6415b99da2f47a5d1b6c3995
haozhe-sun <haozhe.sun@telecom-paristech.fr>
2017-01-29 18:44:17 +0100

    Creation of class of test for the sensors

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/Tests/EmulatorAccelerometer.java
@@ -0,0 +1,7 @@
+package com.example.raphaelattali.rythmrun.Tests;
+
+
+
+public class EmulatorAccelerometer {
+    //TODO
+}
---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/Tests/TestAccelerometer.java
@@ -0,0 +1,6 @@
+package com.example.raphaelattali.rythmrun.Tests;
+
+
+public class TestAccelerometer {
+    //TODO
+}
