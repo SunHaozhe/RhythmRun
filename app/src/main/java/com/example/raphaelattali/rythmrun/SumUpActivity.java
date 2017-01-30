da212dcc683b13c02591473b8efd4f871ab32e2b
Saousan KADDAMI <saousan.kaddami@telecom-paristech.fr>
2017-01-30 15:27:58 +0100

    Création de l'activité donnant le Sum up de la course, dans un sample aléatoire. (Reste à rajouter un lien pour cette activité)

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/SumUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.raphaelattali.rythmrun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SumUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sum_up);
+    }
+}
