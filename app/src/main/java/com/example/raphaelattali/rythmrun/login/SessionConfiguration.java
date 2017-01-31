5d7e802270c780062035a10931e7c356868de190
Raphael Attali <raphael.attali@telecom-paristech.fr>
2017-01-31 13:50:04 +0100

    Classe implémentée permettant de gérer les sessions utilisateur

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/login/SessionConfiguration.java
@@ -0,0 +1,55 @@
+package com.example.raphaelattali.rythmrun.login;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+/**
+ * Created by Raphael Attali on 31/01/2017.
+ * <p>
+ * Project RythmRun
+ */
+
+/**
+ * Permet d'ouvrir, de fermer ou de connaitre l'état de la session utilisateur
+ */
+public class SessionConfiguration {
+        private static String TAG = SessionConfiguration.class.getSimpleName();
+
+        SharedPreferences preferences;
+        SharedPreferences.Editor editor;
+        Context context;
+
+        private static final String PREFERENCES_NAME = "UpdityLogin";
+        private static final String KEY_LOGGED_IN = "isLoggedIn";
+
+    /**
+     * Constructeur de SessionConfiguration
+     * @param context Le contexte de l'application (utilisez getApplicationContext())
+     */
+    public SessionConfiguration(Context context){
+            this.context = context;
+            preferences = context.getSharedPreferences(PREFERENCES_NAME,Context.MODE_PRIVATE);
+            editor = preferences.edit();
+        }
+
+    /**
+     * Actualiser l'état de la session
+     * @param isLoggedIn
+     */
+    public void setLogin(boolean isLoggedIn){
+            editor.putBoolean(KEY_LOGGED_IN,isLoggedIn);
+            editor.commit();
+            Log.i(TAG,"Etat de la session modifiée");
+        }
+
+    /**
+     * Retourne si l'utilisateur a une session ouverte.
+     * @return true si une session user est ouverte.
+     */
+    public boolean isLoggedIn(){
+            return preferences.getBoolean(KEY_LOGGED_IN,false);
+        }
+
+}
+
