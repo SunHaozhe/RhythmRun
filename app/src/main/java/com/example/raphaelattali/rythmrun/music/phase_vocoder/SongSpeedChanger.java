4bfb7e848f5e2a0d24f0184ec2ffb76da034c6cc
Raphael Attali <raphael.attali@telecom-paristech.fr>
2017-01-31 19:17:10 +0100

    Ajout de 2 classes pour la première étape de la modification de tempo

---
+++ app/src/main/java/com/example/raphaelattali/rythmrun/music/phase_vocoder/SongSpeedChanger.java
@@ -0,0 +1,17 @@
+package com.example.raphaelattali.rythmrun.music.phase_vocoder;
+
+/**
+ * Created by Raphael Attali on 31/01/2017.
+ * <p>
+ * Project RythmRun
+ */
+
+
+/**
+ * Classe permettant d'accélérer/de ralentir une chanson. Attention, le résultat ne sera pas agréable à l'oreille si
+ * les différences de vitesses sont de plus de 20-30%
+ */
+public class SongSpeedChanger {
+
+
+}
