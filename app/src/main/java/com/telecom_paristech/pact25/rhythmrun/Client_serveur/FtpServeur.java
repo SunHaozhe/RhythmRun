17d3b3718745efdc48bbd204a48ff53ab0bbb715
haozhe-sun <haozhe.sun@telecom-paristech.fr>
2017-02-26 19:02:45 +0100

    Creation of FtpServeur

---
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Client_serveur/FtpServeur.java
@@ -0,0 +1,31 @@
+package com.telecom_paristech.pact25.rhythmrun.Client_serveur;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
+
+public class FtpServeur {
+	public static void main(String[] args) { 
+        upload(); 
+        download(); 
+    }
+
+	/**
+	 * FTP downloads a single file
+	 */
+	private static void download() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * FTP uploads a single file
+	 */
+	private static void upload() {
+		// TODO Auto-generated method stub
+		
+	} 
+}
