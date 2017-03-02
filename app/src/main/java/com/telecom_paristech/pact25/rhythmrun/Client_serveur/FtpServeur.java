c713f87ce9c5423bdd703002f941d7fe04fe1a0d
haozhe-sun <haozhe.sun@telecom-paristech.fr>
2017-03-02 16:54:36 +0100

    Re-realization of FTP server (not completed)

--- app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Client_serveur/FtpServeur.java
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Client_serveur/FtpServeur.java
@@ -1,91 +1,60 @@
 package com.telecom_paristech.pact25.rhythmrun.Client_serveur;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-import org.apache.commons.net.ftp.FTPClient; 
+import com.telecom_paristech.pact25.rhythmrun.R;
+import android.view.View.OnClickListener;
+import com.telecom_paristech.pact25.rhythmrun.Client_serveur.sauronsoftware.ftp4j.*;
+
 
 /**
- * traits the FTP 
+ * traits the FTP
  *
  *
  */
-public class FtpServeur {
-	
-	/**
-	 * FTP uploads a single file
-	 * @throws IOException 
-	 */
-	public static void upload() throws IOException{
-		FTPClient ftpClient = new FTPClient(); 
-        FileInputStream fis = null; 
-        
-        try { 
-            ftpClient.connect(""); //TODO enter TP address
-            ftpClient.login("", ""); //TODO "enter login", "enter password"
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
+public class FtpServeur extends AppCompatActivity implements OnClickListener {
+    /*********
+     * work only for Dedicated IP
+     ***********/
+    static final String FTP_HOST = "XX.XX.XX.XX";
 
-            String fileName = ""; //TODO enter the name of the file
-            File srcFile = new File(""); //TODO enter the path of the file
-            fis = new FileInputStream(srcFile); 
-            
-            //Setup of the upload directory
-            ftpClient.changeWorkingDirectory(""); //TODO enter the path of the working directory
-            ftpClient.setBufferSize(1024); 
-            ftpClient.setControlEncoding("GBK"); 
-            
-            //Sets up file type to binary
-            ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE); 
-            ftpClient.storeFile(fileName, fis);
-            
-        } catch (IOException e) { 
-            e.printStackTrace(); 
-            throw new RuntimeException("FTP client has an error", e); 
-        } finally { 
-            fis.close(); 
-            try { 
-                ftpClient.disconnect(); 
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-                throw new RuntimeException("An exception occured while we disconnect the FTP", e); 
-            } 
-        } 
-	}
-	
-	/**
-	 * FTP downloads a single file
-	 * @throws IOException 
-	 */
-	public static void download() throws IOException{
-		FTPClient ftpClient = new FTPClient(); 
-        FileOutputStream fos = null; 
-        
-        try { 
-            ftpClient.connect(""); //TODO enter TP address
-            ftpClient.login("", ""); //TODO "enter login", "enter password"
+    /*********
+     * FTP USERNAME
+     ***********/
+    static final String FTP_USER = "XXXXXX";
 
-            String remoteFileName = ""; //TODO enter the remote file name with the path of directory
-            fos = new FileOutputStream(""); //TODO enter the name of the file with its path 
-            ftpClient.setBufferSize(1024); 
-            
-            //Sets up the file type to binary
-            ftpClient.setFileType(FTPClient.BINARY_FILE_TYPE); 
-            ftpClient.retrieveFile(remoteFileName, fos); 
-            
-        } catch (IOException e) { 
-            e.printStackTrace(); 
-            throw new RuntimeException("FTP client has an error", e); 
-        } finally { 
-            fos.close(); 
-            try { 
-                ftpClient.disconnect(); 
-            } catch (IOException e) { 
-                e.printStackTrace(); 
-                throw new RuntimeException("An exception occured when we disconnect the FTP", e); 
-            } 
-        } 
-		
-	}
+    /*********
+     * FTP PASSWORD
+     ***********/
+    static final String FTP_PASS = "XXXXXX";
 
+    Button upload_btn, download_btn;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ftpserveur);
+        try {
+            upload_btn = (Button) findViewById(R.id.button_send_ftp);
+            upload_btn.setOnClickListener(this);
+            download_btn = (Button) findViewById(R.id.button_download_ftp);
+            download_btn.setOnClickListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
