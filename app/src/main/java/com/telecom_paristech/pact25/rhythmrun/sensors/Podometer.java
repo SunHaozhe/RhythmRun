882ee14c68d9e92d6dc4de3613eb37550d65b6a4
Saousan KADDAMI <saousan.kaddami@telecom-paristech.fr>
2017-03-04 23:21:49 +0000

    Podometer checking

--- app/src/main/java/com/telecom_paristech/pact25/rhythmrun/sensors/Podometer.java
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/sensors/Podometer.java
@@ -39,7 +39,8 @@ public class Podometer implements PodometerInterface, SensorEventListener {
         numberOfValues = acc.getNombreEchantillons();
         values = new float[3][numberOfValues];
         accValues = acc.getArray();
-        while (!acc.isActive()) {
+        while (!acc.isActive())
+        {
             sleep(500);
             Log.i("lucas", "attente...");
         }
