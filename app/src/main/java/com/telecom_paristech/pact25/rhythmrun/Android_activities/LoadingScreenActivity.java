a9f0ac085b292cdf61b5663b60ff24b2e820128e
Yohan Chalier <yohan.chalier@telecom-paristech.fr>
2017-03-09 22:10:23 +0100

    Création de l'activité LoadingActivity.

---
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Android_activities/LoadingScreenActivity.java
@@ -0,0 +1,18 @@
+package com.telecom_paristech.pact25.rhythmrun.Android_activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.telecom_paristech.pact25.rhythmrun.R;
+
+public class LoadingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+    }
+}
