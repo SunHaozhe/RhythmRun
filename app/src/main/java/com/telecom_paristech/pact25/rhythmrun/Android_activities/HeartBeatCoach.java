980eba5bc61b6e2216d0b024d13425b91dad1c63
Saousan KADDAMI <saousan.kaddami@telecom-paristech.fr>
2017-04-20 14:06:29 +0100

    Constructors of the HeartBeat class and attributes

--- app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Android_activities/HeartBeatCoach.java
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Android_activities/HeartBeatCoach.java
@@ -5,4 +5,13 @@ package com.telecom_paristech.pact25.rhythmrun.Android_activities;
  */
 
 public class HeartBeatCoach {
+    int age;
+    int gender;
+
+    public HeartBeatCoach(int age, int gender)
+    {
+        this.age = age;
+        this.gender = gender;
+    }
+    
 }
