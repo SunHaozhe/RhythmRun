6e5906717f07dcee49b9022471a222d4a597667b
Saousan KADDAMI <saousan.kaddami@telecom-paristech.fr>
2017-04-20 14:35:14 +0100

    Comments to explicite the function of each function created in the HeartBeatCoach Class

--- app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Android_activities/HeartBeatCoach.java
+++ app/src/main/java/com/telecom_paristech/pact25/rhythmrun/Android_activities/HeartBeatCoach.java
@@ -7,6 +7,11 @@ package com.telecom_paristech.pact25.rhythmrun.Android_activities;
 public class HeartBeatCoach {
     int age;
     int gender;
+    int frequencyAtRest;
+    int frequencyAfterRun;
+
+    //frequencyAtRest is the value of HeartBeat measured when the user is resting, before the run or hours after it
+    //frequencyAfterRun is the value of frequency measured one minut after the runner stops running
 
     public HeartBeatCoach(int age, int gender)
     {
@@ -14,6 +19,8 @@ public class HeartBeatCoach {
         this.gender = gender;
     }
 
+
+    //Age and gender are to be setteled from the share preferences or the data basis:
     public int getAge()
     {
         return 20;
@@ -24,13 +31,28 @@ public class HeartBeatCoach {
         return 220;
     }
 
+    //FrequencyAtRest should be returned by the chest strap when the runner is at rest (anytime, not during the run)
+    public int getFrequencyAtRest()
+    {
+        return 65;
+
+    }
+
+    //FrequencyAfterRun should be returned by the chest strap about 1 minute after the runner decides to stop
+    public int getFrequencyAfterRun()
+    {
+        return 100;
+
+    }
+
+    //HeartBeatFrequency should be returned by the chest strap during the run
     public int getHeartBeatFrequency()
     {
         return 140;
     }
 
-    //Calculate the maximum value of Heartbeat
 
+    //Calculate the maximum theorical value of Heartbeat
     public int getFCM()
     {
         return gender-age;
@@ -45,19 +67,21 @@ public class HeartBeatCoach {
 
     }
 
+    //Indicates whether the frequency of heart beat is good or not
     public boolean goodHeartBeatFrequency()
     {
         if (getHeartBeatFrequency()<getFC())
         {
-            return True;
+            return true;
         }
         else
         {
-            return False;
+            return false;
 
         }
     }
 
+    //Returns a message to the runner according to the accuracy of his run
     public String messageCoaching()
     {
         if (goodHeartBeatFrequency())
